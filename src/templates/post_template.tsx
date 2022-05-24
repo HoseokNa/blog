cbf3543807b4d2ef5bd77d6acd24873f362fb952
Hoseok Na <hsna7024@gmail.com>
2022-05-24 23:03:00 +0900

    feat: Create Post

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,13 +1,56 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
+import { PostFrontmatterType } from 'types/PostItem.types'
 import Template from 'components/Common/Template'
+import PostHead from 'components/Post/PostHead'
+import PostContent from 'components/Post/PostContent'
 
-type PostTemplateProps = {}
+export type PostPageItemType = {
+  node: {
+    html: string
+    frontmatter: PostFrontmatterType
+  }
+}
 
-const PostTemplate: FunctionComponent<PostTemplateProps> = function (props) {
-  console.log(props)
+type PostTemplateProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[] // 존재하지 않는 타입이므로 에러가 발생하지만 일단 작성해주세요
+    }
+  }
+}
 
-  return <Template>Post Template</Template>
+const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
+  const {
+    node: {
+      html,
+      frontmatter: {
+        title,
+        summary,
+        date,
+        categories,
+        thumbnail: {
+          childImageSharp: { gatsbyImageData },
+        },
+      },
+    },
+  } = edges[0]
+
+  return (
+    <Template>
+      <PostHead
+        title={title}
+        date={date}
+        categories={categories}
+        thumbnail={gatsbyImageData}
+      />
+      <PostContent html={html} />
+    </Template>
+  )
 }
 
 export default PostTemplate
