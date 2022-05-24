54ace332df39f90f78d7cf30eaf02aa7e1bcd27e
Hoseok Na <hsna7024@gmail.com>
2022-05-24 22:08:15 +0900

    feat: Add slug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -99,6 +99,9 @@ export const getPostList = graphql`
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             summary
