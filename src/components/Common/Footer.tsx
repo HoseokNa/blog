827470d0e58690f8c65e14f71c9fb67ad306586e
Hoseok Na <hsna7024@gmail.com>
2022-06-27 10:37:57 +0900

    feat: Add github, til icons in footer

--- src/components/Common/Footer.tsx
+++ src/components/Common/Footer.tsx
@@ -1,5 +1,8 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
 
 const FooterWrapper = styled.footer`
   display: grid;
@@ -14,12 +17,29 @@ const FooterWrapper = styled.footer`
     font-size: 13px;
   }
 `
+const IconWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+  margin-top: 8px;
+`
 
 const Footer: FunctionComponent = function () {
   return (
     <FooterWrapper>
       Thank You for Visiting My Blog, Have a Good Day 😆
       <br />© {new Date().getFullYear()} Developer Hoseok, Powered By Gatsby.
+      <IconWrapper>
+        <a href="https://github.com/hoseokna" target="_blank">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a
+          href="https://til-hoseok.notion.site/TIL-f69a873e19b545f8a9c39b99331d7e0c"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faBook} />
+        </a>
+      </IconWrapper>
     </FooterWrapper>
   )
 }
